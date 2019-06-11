4aff18a71cb59a6cacb4b70278f9da08e2df64c2
Genesis Ortez <gdaniortez@gmail.com>
2019-06-11 15:28:21 -0600

    2do

--- routes/api/personas.js
+++ routes/api/personas.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+var personaCollecction =[];
 
+var personaStruct= {
+    id:0,
+    nombre:'',
+    correo:'',
+    telefono:'',
+    nota:''
 
+}
+
+router.get('/', (req,res, next)=>{
+res.status(403).json({msg:"not implemented"});
+});
+
+router.get('/:id', (req,res,nect)=>{
+    res.status(403).json({msg:"not implemented"});
+})//get one by id
+
+router.post('/', (req,res,next)=>{
+    res.status(403).json({msg:"not implemented"});
+});
+
+router.put('/:id', (req,res,next)=>{
+    res.status(403).json({msg:"not implemented"});
+});
+
+router.delete('/:id', (req,res,next)=>{
+    res.status(403).json({msg:"not implemented"});
+});
 
 
 module.exports= router;
